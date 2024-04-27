bb9704274d1e619063fc7167cfc32d93370d81f7
sunita <sunitaraut623@gmail.com>
2024-04-27 20:54:30 +0545

    commit

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,46 +1,3 @@
-/*import React, { useState } from 'react';
-import './Login.css';
-
-const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handleEmailChange = (event) => {
-    setEmail(event.target.value);
-  };
-
-  const handlePasswordChange = (event) => {
-    setPassword(event.target.value);
-  };
-
-  const handleSubmit = () => {
-    // Handle login submission here
-    console.log("Email:", email);
-    console.log("Password:", password);
-  };
-
-  return (
-    <div className='container'>
-      <div className='header'>
-        <div className='text'>Login</div>
-        <div className='underline'></div>
-      </div>
-      <div className='inputs'>
-        <div className='input'>
-          <input type='email' placeholder='Email' value={email} onChange={handleEmailChange} />
-        </div>
-        <div className='input'>
-          <input type='password' placeholder='Password' value={password} onChange={handlePasswordChange} />
-        </div>
-      </div>
-      <div className='submit-container'>
-        <div className='submit' onClick={handleSubmit}>Login</div>
-      </div>
-    </div>
-  );
-};
-
-export default Login;*/
 
 import React, { useState } from 'react';
 import './Login.css'; // Import CSS file for styling
@@ -56,11 +13,26 @@ function Login() {
     const handlePasswordChange = (e) => {
         setPassword(e.target.value);
     };
-
     const handleSubmit = (e) => {
-        e.preventDefault();
-        // Handle login logic here
-    };
+      e.preventDefault();
+  
+      fetch('http://localhost/PHP%20BACKEND/login.php', {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ username, password }),
+      })
+      .then(response => response.text())
+      .then(data => {
+          console.log(data);
+          // Handle response from server
+      })
+      .catch(error => {
+          console.error('Error:', error);
+      });
+  };
+  
 
     return (
         <div className="login-container">
